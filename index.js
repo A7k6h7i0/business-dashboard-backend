0d93c35c4f546aa772d5ea44a9a5e7240168f2a8
A7k6h7i0 <148232686+A7k6h7i0@users.noreply.github.com>
2025-07-06 13:45:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+      origin: 'https://business-dashboard-frontend-1p0c.onrender.com/'
+    }));
 app.use(express.json());
 
 const PORT = 5000;
